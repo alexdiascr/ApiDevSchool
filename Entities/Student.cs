2a1bcb53f21dba4c20cb9eed7416863ea386d25e
alexdiascr <git config --global user.email eng.alexdiasc@gmail.comgit config --global user.email>
2023-09-24 18:48:38 -0300

    first commit

---
+++ Entities/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace DevSchool.Entities
+{
+    public class Student
+    {
+        //Construtor vazio para facilitar o mapeamento do dapper
+        public Student() {}
+
+        public Student(string fullName, DateTime birthDate, string schoolClass)
+        {
+            FullName = fullName;
+            BirthDate = birthDate;
+            SchoolClass = schoolClass;
+            IsActive = true;
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string SchoolClass { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Adicionando suporte a Diversos arquivos de configuração por ambiente.
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
